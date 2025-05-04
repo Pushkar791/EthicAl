8064b49ad7839bdb7698dbf4c7148236bf8e5cd8
User <user@example.com>
2025-05-05 02:51:51 +0530

    deployment issue fixed

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,7 @@
-from app import create_app, db
-from app.models import *  # Import all models for migration
+from app import create_app
 
 app = create_app()
 
-# This is important for Vercel serverless
+# Handler for Vercel serverless environment
 def handler(request, **kwargs):
-    try:
-        # For first run only, try to create tables
-        # This may fail if the app is already created, which is fine
-        if request.get('path', '') == '/':
-            with app.app_context():
-                try:
-                    db.create_all()
-                except:
-                    pass  # Silently continue if tables already exist
-    except:
-        pass  # Fail silently if there are issues
-
     return app(request.environ, request.start_response) 
